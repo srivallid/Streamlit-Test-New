3d3891de8c890e7b0e35ccb0d0fb84af26838463
srivallid <srivalli.dingari@gmail.com>
2022-11-21 16:21:20 -0500

    update code

--- pages/Page1.py
+++ pages/Page1.py
@@ -4,12 +4,14 @@ from password_check import check_password
 import os
 import zipfile
 import pickle
+import janitor
 
 if check_password():
     st.set_page_config(page_title="Page1", page_icon="⚙️")
     st.write("This is a test page")
     df = pd.DataFrame({'a' : [1,2,3,4,5,6],
                        'b' : [2,3,4,5,6,7]})
+    df = df.clean_names()
     df
     archive = zipfile.ZipFile('models//testregmodel.zip', 'r')
     lr = pickle.loads(archive.open('testregmodel.sav').read())
